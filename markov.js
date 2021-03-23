1db302f8ee05a003197a4ca6ea22910241ecb934
Kat Huang <khuang129@gmail.com>
2021-03-23 14:33:40 -0700

    initial commit

---
+++ markov.js
@@ -0,0 +1,28 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    // MORE CODE HERE
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    // MORE CODE HERE
+  }
+
+
+  /** return random text from chains */
+
+  getText(numWords = 100) {
+    // MORE CODE HERE
+  }
+}
