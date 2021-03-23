969e9fa69536d3fedae95a8826912dddb5b4bb7a
Kat Huang <khuang129@gmail.com>
2021-03-23 15:34:00 -0700

    made makeChains method, now need to implement getText method in class

--- markov.js
+++ markov.js
@@ -1,22 +1,41 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
+    this.chain = this.makeChains(words);
     // MORE CODE HERE
   }
 
   /** set markov chains:
    *
    *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+   *  {"the": ["cat", "hat", "the"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // MORE CODE HERE
+  //["the", "cat", "the"]
+  //{"the": ["cat", "undef"], "cat": ["the"], }
+
+  makeChains(words) {
+    let chain = {};
+    for(let i = 0; i < words.length; i++) {
+      if (chain[words[i]] === undefined) { 
+        if (i === words.length - 1) {
+          chain[words[i]] = [null];
+        } else {
+          chain[words[i]] = [words[i + 1]];
+        }
+      } else {
+        if (i === words.length - 1)  {
+          chain[words[i]].push(null);
+        } else {
+          chain[words[i]].push(words[i + 1]);
+        }
+      } 
+    }
+    return chain;
   }
 
 
@@ -26,3 +45,5 @@ class MarkovMachine {
     // MORE CODE HERE
   }
 }
+
+module.exports = {MarkovMachine}
